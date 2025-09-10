2c3a01bbf6b925408944b19fa1bcf3abd0c82e30
Sujith <sujith.sappani@gmail.com>
2025-09-10 15:22:19 +0530

    UI Check

--- Backend/src/main/java/com/crud/Backend/BackendApplication.java
+++ Backend/src/main/java/com/crud/Backend/BackendApplication.java
@@ -1,38 +1,12 @@
 package com.crud.Backend;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
-@RestController
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String getMethodName() {
-		System.out.println("heloo");
-		return "Sujith websote ";
-	}
-
-	@GetMapping("/details")
-	public List<card> getNames() {
-		List<card> list = new ArrayList<>();
-		list.add(new card("Sujit ", 21));
-		list.add(new card("kamal", 31));
-		list.add(new card("Rohit ", 47));
-
-		System.out.println("Hola");
-		System.out.println(list.get(0).toString());
-		return list;
-	}
-
 }
---
+++ Backend/src/main/java/com/crud/Backend/Controller.java
@@ -0,0 +1,13 @@
+package com.crud.Backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    
+}
